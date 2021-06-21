d4d3e4460c8384cf9438ffffd56504d4d5779a21
Hyunsoo-Ryan-Lee <83285291+Hyunsoo-Ryan-Lee@users.noreply.github.com>
2021-06-21 23:50:32 +0900

    Update test2.py

--- JWC/test2.py
+++ JWC/test2.py
@@ -45,6 +45,5 @@ def login():
 
     return make_response('bad',401,{'WWW-Authenticate' : 'Basic realm="Login Required"'})
 
-
 if __name__ == '__main__':
     app.run(debug = True, host="127.0.0.1", port="5000" )
